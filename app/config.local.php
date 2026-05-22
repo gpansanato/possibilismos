520c2412b1bf9f8bb925aaab83bc3ecbc6998001
gpansanato <gpansanato@outlook.com>
2026-05-22 11:29:50 -0300

    Update database and cron configuration settings

---
+++ app/config.local.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'mysql.panblan.com.br',
+        'name' => 'panblan',
+        'user' => 'panblan',
+        'pass' => 'meuposs1bilism0',
+    ],
+
+    'cron' => [
+        'token' => '12312312312312312312312312312312a3333734534',
+    ],
+];
